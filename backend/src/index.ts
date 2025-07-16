71033edc98955f206e31379b5105e0cb280f5822
Oscar Stickland <76094129+oscarstickland@users.noreply.github.com>
2025-07-16 21:08:08 +1000

    Added Prisma to project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
 
 const app = express();
 const port = process.env.PORT || 3000;
+const prisma = new PrismaClient()
 
 app.use(express.json());
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/", async (req: express.Request, res: express.Response) => {
     res.status(200).send("Hello");
 });
 
